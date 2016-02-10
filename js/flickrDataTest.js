0c46a8da0529b92ffc8860c6f69f6c4f15ca579d
jean-michel.legrand <Fluent2015>
2016-02-10 17:46:43 +0100

    Add a sample demo using flickr API

--- js/flickrDataTest.js
+++ js/flickrDataTest.js
@@ -22,42 +22,70 @@
 // https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key=983c56b3652c1b44c58ddce47cef6449&user_id=138726948@N08&format=json&per_page=5
 
 
-//var  apiKey = b3bdddc89ecc48e025bfad40ac785142;
-var userId = '138726948@N08';
-
-var flickrCall = function () {
+var flickrCall = function() {
   //var url = 'https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key=' + apiKey + '&user_id=' + userId + '&format=json&jsoncallback=?';
 
-
-  var tags = 'Roland';
+  var apiKey = 'b3bdddc89ecc48e025bfad40ac785142';
+  var userId = '138726948@N08';
+  var tagsInput = 'Roland Garros';
   var tagmode = 'and';
   var lat = 48.86342;
   var long = 2.26189;
-  var radius = 1;
+  var radius = 2;
 
-  //var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + apiKey + '&tags=' + tags + '&tag_mode=' + tagmode + '&lat=' + lat + '&lon=' + long + '&radius=' + radius + '&radius_units=km&format=json&nojsoncallback=1';
+  var tags = _.words(tagsInput);
+  var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + apiKey + '&tags=' + tags + '&tag_mode=' + tagmode + '&lat=' + lat + '&lon=' + long + '&radius=' + radius + '&radius_units=km&format=json&nojsoncallback=1';
+  console.log(url);
 
-  var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=b3bdddc89ecc48e025bfad40ac785142&tags=Roland+Garros&tag_mode=and&lat=48.86342&lon=2.26189&radius=2&radius_units=km&format=json&nojsoncallback=1';
+  //var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=b3bdddc89ecc48e025bfad40ac785142&tags=Roland+Garros&tag_mode=and&lat=48.86342&lon=2.26189&radius=2&radius_units=km&format=json&nojsoncallback=1';
 
   $.getJSON(url)
-    .success(function (data) {
+    .success(function(data) {
       console.log('yessss');
       console.log(data);
 
       //loop through the results with the following function
-      $.each(data.photos.photo, function (i, item) {
+      var filteredData = _.filter(data.photos.photo, function(photo) {
+        var match = false;
+        // check if title is different from ""
+        if (!_.isEmpty(photo.title)) {
 
-        // take the first one and store the value
+          // tranform string into array
+          var words = _.words(photo.title);
 
-
-        //build the url of the photo in order to link to it
-        var photoURL = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_m.jpg';
-      })
+          return _.isEqual((_.filter(tags, function(tag) {
+            var res = _.findIndex(words, function(word) {
+              return word === tag
+            });
+            match = (res === -1) ? false : true;
+            return match;
+          })), tags);
+        }
+      });
+      console.log(filteredData);
     })
-    .fail(function (e) {
+    .fail(function(e) {
       console.log('nooooo...%o', e)
     });
   console.log('sent');
 };
 
 flickrCall();
+
+/*
+var elementsATrouver = ["Roland", "Garros"];
+var inputs = [["Serena", "Garros", "Roland", "2015"], ["Serena", "Wiliams"], ["Roland", "Garros", "2005"]];
+
+var goodCandidate  = _.isEqual((_.filter(elementsATrouver, function(elementATrouver) {
+  var found = false;
+  var res = _.findIndex(inputs, function(element) {
+    return element === elementATrouver
+  });
+  found = (res === -1) ? false : true;
+  return found;
+})), elementsATrouver);
+
+if (goodCandidate) {
+  console.log("element found!!")
+}
+*/
